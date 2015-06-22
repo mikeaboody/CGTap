22f7b0b823c65d8349204146a4c5499477997850
Michael Aboody <michael@Control-Group-Employee-3.local>
2015-06-22 16:21:57 -0400

    moved configurations to Main.js

--- public/script/Main.js
+++ public/script/Main.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+	if (typeof master_email !== "undefined" && master_email == "mikeaboody@gmail.com" && window.location.href.indexOf("localhost:4567") > -1) {
+		master_email = "brian.forster@controlgroup.com";
+		base = "https://cgp-api-dev.controlgroup.com";
+	}
 	setup();
 });
