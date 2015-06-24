e1288b83c8544558a08a4681e921b24572835c4b
Michael Aboody <michael@Control-Group-Employee-3.local>
2015-06-24 16:00:44 -0400

    fixed adding janines email

--- public/script/Main.js
+++ public/script/Main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	if (typeof master_email !== "undefined" && (master_email == "mikeaboody@gmail.com" || "janine.harper@controlgroup.com") && window.location.href.indexOf("localhost:4567") > -1) {
+	if (typeof master_email !== "undefined" && (master_email == "mikeaboody@gmail.com" || master_email == "janine.harper@controlgroup.com") && window.location.href.indexOf("localhost:4567") > -1) {
 		master_email = "brian.forster@controlgroup.com";
 		base = "https://cgp-api-dev.controlgroup.com";
 	}
